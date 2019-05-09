c684c2cc38bc47eb2bb29e21fe6eaf467730b52b
Matheus Beck <39171184+matheus-beck@users.noreply.github.com>
2019-05-09 11:43:38 -0300

    Add files via upload

---
+++ unique-string.js
@@ -0,0 +1,14 @@
+// Is unique:  an algorithm to determine if a string has all unique characters
+
+isUnique = function(myString) {
+	if (myString.length > 128) return false;
+    var listChar = [];
+    for (var i = 0; i < myString.length; i++){
+        if (listChar.includes(myString[i])) return false;
+        else listChar.push(myString[i]);
+    }
+    return true;
+}
+
+var stringUnique = isUnique("Ola");
+console.log(stringUnique);
