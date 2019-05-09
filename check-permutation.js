2c6ba72d71c1d04f50c0ff2576a7e1fbdcb3dc06
Matheus Beck <39171184+matheus-beck@users.noreply.github.com>
2019-05-09 14:03:17 -0300

    Add files via upload

---
+++ check-permutation.js
@@ -0,0 +1,23 @@
+// Check Permutation: Given two strings, decide if one is a permutation of the other
+
+isPermutation = function(myString1, myString2) {
+	if (myString1.length != myString2.length) return false;
+    
+    var listChar = [];
+    
+    for (var i = 0; i < myString1.length; i++){
+        listChar.push(myString1[i]);
+    }
+    
+    for (var i = 0; i < myString1.length; i++){
+        if (listChar.includes(myString2[i])) {
+        	var index = listChar.indexOf(myString2[i]);
+            delete listChar[index];
+        }
+        else return false;
+    }
+    return true;
+}
+
+var stringPermutation = isPermutation("cara", "arca");
+console.log(stringPermutation);
