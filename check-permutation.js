bc5b2121a9288226cba29afabeefe758bca35bf7
Matheus Beck <39171184+matheus-beck@users.noreply.github.com>
2019-05-09 14:04:17 -0300

    Update check-permutation.js

--- check-permutation.js
+++ check-permutation.js
@@ -1,17 +1,17 @@
 // Check Permutation: Given two strings, decide if one is a permutation of the other
 
 isPermutation = function(myString1, myString2) {
-	if (myString1.length != myString2.length) return false;
+    if (myString1.length != myString2.length) return false;
     
     var listChar = [];
-    
+	
     for (var i = 0; i < myString1.length; i++){
         listChar.push(myString1[i]);
     }
     
     for (var i = 0; i < myString1.length; i++){
         if (listChar.includes(myString2[i])) {
-        	var index = listChar.indexOf(myString2[i]);
+            var index = listChar.indexOf(myString2[i]);
             delete listChar[index];
         }
         else return false;
