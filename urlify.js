8d3da9f0bdce5b165099234f2b1edc29c2db032b
Matheus Beck <matheus.alencarbeck@gmail.com>
2019-05-10 14:08:19 -0300

    Update urlify.js

--- urlify.js
+++ urlify.js
@@ -1,9 +1,8 @@
 // URLify: Write a method to replace all spaces in a string with '%20'
 
-urlify = function(myString, length){
-	return myString.replace(/ /g, "%20");
+urlify = function(myString){
+    return myString.replace(/ /g, "%20");
 }
 
-var myTest = "Let's test this string "
-var result = urlify(myTest, myTest.length);
-console.log(result);
+var test = urlify("Let's test this string ");
+console.log(test);
